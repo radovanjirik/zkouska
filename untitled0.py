7235331c6d9e038686d68d8a270bcbedef406fb4
RJ\Radovan Jiřík <jirik@isibrno.cz>
2020-01-06 11:01:09 +0100

    přidal jsem untitled0.py

---
+++ untitled0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 10:58:30 2020
+
+@author: Radovan Jiřík
+"""
+
+from scipy import optimize    #### https://docs.scipy.org/doc/scipy/reference/optimize.html - pro základní optimalizaci, pro komplexnější bych volil balíček cvxopt pro convexní problémy, gurobi pro nekonvexní 
+import numpy as np
+import matplotlib.pyplot as plt
+
+#zk
